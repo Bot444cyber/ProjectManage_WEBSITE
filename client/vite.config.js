fa9641398f23e35ccf84c29768773e5ed9c055c3
CyberBot444 <ar1777332@gmail.com>
2025-06-24 11:40:03 +0500

    Your commit message here

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:1000',
+        target: `${import.meta.env.BACKEND_URL}`,
       }
     }
   }
