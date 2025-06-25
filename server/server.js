0acf8135278cb720d186c042cb94ce1ea3daa285
CyberBot444 <ar1777332@gmail.com>
2025-06-25 13:32:43 +0500

    Cros Configration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -11,27 +10,27 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS Configuration
+mongoose.connect(
+    process.env.URL_MONGOOSE, {
+        dbName: process.env.DB_NAME
+    }
+).then(() => {
+    console.log('DB is connected.')
+}).catch(() => {
+    console.log('Error occure during DB Connection.')
+});
+
 const corsOptions = {
-  origin: process.env.URL_FRONTEND,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+    origin: [
+      'client-three-sepia-49.vercel.app',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
 };
 app.use(cors(corsOptions));
 
-// MongoDB Connection
-mongoose.connect(
-  process.env.URL_MONGOOSE,
-  { dbName: process.env.DB_NAME }
-).then(() => {
-  console.log('DB is connected.');
-}).catch((err) => {
-  console.log('Error during DB connection:', err.message);
-});
-
-// Routes
 app.get('/', (req, res) => { res.send('Welcome to API.') });
 app.use('/api', router);
 
-// Server Start
-export default app;
+app.listen(PORT);
